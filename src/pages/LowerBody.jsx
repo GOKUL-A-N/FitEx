6c3fe46ff7640073e1a513700ef212cee7c9fcab
GOKUL-A-N <gokulcricketer141103@gmail.com>
2023-05-15 15:15:18 +0530

    minor changes in lowerbody.jsx

--- src/pages/LowerBody.jsx
+++ src/pages/LowerBody.jsx
@@ -3,8 +3,8 @@ import {Link , Outlet} from "react-router-dom"
 
 const LowerBody = () => {
   return (
-    <div className='overflow-auto '>
-        <div className='flex-1 justify-center items-center'>
+    <div className='h-screen overflow-auto '>
+        <div className='flex-1 justify-center items-center mb-10'>
         <h1 className='p-[50px] text-center text-main'>LOWERBODY WORKOUTS</h1>
         <div className='flex-1 flex flex-col justify-center items-center'>
         <Link to="/LowerBody/calf"><button className='Upper-button'>Calf</button></Link>
@@ -13,6 +13,7 @@ const LowerBody = () => {
         <Link to="/LowerBody/quads"><button className='Upper-button'>Quads</button></Link>
         </div>
         </div>
+        <hr className='w-[100%] h-3 flex justify-center self-center'/>
         <Outlet />
     </div>
   )
